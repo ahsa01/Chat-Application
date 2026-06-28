a61782d87c66fd01658dc97d19ede1f6dd152eb7
Ashajyothi <ashajyothibandaru2005@gmail.com>
2026-06-28 23:31:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const {Server} = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io=new Server(server);
+
+//handle socket io 
+io.on('connection', (socket) =>{
+    socket.on('user-message',message =>{
+        //send to all connections
+        io.emit('message',message);
+    })
+    console.log('A new user has connected',socket.id)
+
+})
+
+
+app.use(express.static(path.resolve("./public")));
+app.get('/',(req,res) =>{
+    return res.sendFile("./public/index.html");
+});
+
+server.listen(9000,()=>
+console.log(`server started at PORT:9000`));
+
